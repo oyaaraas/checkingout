7d427f0d0e30db9ed16a8d071f44a2f5bae1a923
Øyvind Aaraas <Oyvind.Aaraas@klp.no>
2016-12-17 18:51:23 +0100

    chore: bootstrap project

---
+++ src/client/app/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
+import {Button, ButtonToolbar, Col, Grid, Row, Jumbotron, PageHeader, Panel} from 'react-bootstrap';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Grid>
+                    <PageHeader>checkingout.com
+                        <small>&nbsp;freelance</small>
+                    </PageHeader>
+                    <Row className="show-grid">
+                        <Col xs={12} md={4}>
+                            <ButtonToolbar>
+                                {/* Standard button */}
+                                <Button>Default</Button>
+
+                                {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                                <Button bsStyle="primary">Primary</Button>
+
+                                {/* Indicates a successful or positive action */}
+                                <Button bsStyle="success">Success</Button>
+
+                                {/* Contextual button for informational alert messages */}
+                                <Button bsStyle="info">Info</Button>
+
+                                {/* Indicates caution should be taken with this action */}
+                                <Button bsStyle="warning">Warning</Button>
+
+                                {/* Indicates a dangerous or potentially negative action */}
+                                <Button bsStyle="danger">Danger</Button>
+
+                                {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+                                <Button bsStyle="link">Link</Button>
+                            </ButtonToolbar>
+
+                            <AwesomeComponent/>
+
+                        </Col>
+                        <Col xs={6} md={8}>
+                            <Jumbotron>
+                                <h1>checkingout.com!</h1>
+                                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra
+                                    attention to featured content or information.</p>
+                                <p><Button bsStyle="primary">Learn more</Button></p>
+                            </Jumbotron>
+                        </Col>
+                    </Row>
+                    <Row className="show-grid">
+                        <Col xs={8} md={6}>
+                            <Panel header="Person" bsStyle="info">
+                                <Button bsSize="large" block>Meld deg på her</Button>
+                            </Panel>
+                        </Col>
+                        <Col xs={8} md={6}>
+                            <Panel header="Bedrift" bsStyle="success">
+                                <Button bsSize="large" block>Meld deg på her</Button>
+                            </Panel>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    entry: [
+        APP_DIR + '/index.jsx'
+    ],
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: "/assets/"
+    },
+    module : {
+        loaders : [
+            {
+                test: /\.jsx?/,
+                loader: 'babel',
+                include: APP_DIR
+            }
+        ]
+    }
+};
+
+module.exports = config;
