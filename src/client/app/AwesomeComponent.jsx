54eafcc41fbe58e6e96c00fdd9d15eb4f48b9786
Øyvind Aaraas <Oyvind.Aaraas@klp.no>
2016-12-18 02:13:35 +0100

    style: update

--- src/client/app/AwesomeComponent.jsx
+++ src/client/app/AwesomeComponent.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 
 class AwesomeComponent extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {likesCount : 5};
-        this.onLike = this.onLike.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {likesCount: 5};
+    this.onLike = this.onLike.bind(this);
+  }
 
-    onLike () {
-        let newLikesCount = this.state.likesCount + 1;
-        this.setState({likesCount: newLikesCount});
-    }
+  onLike() {
+    let newLikesCount = this.state.likesCount + 1;
+    this.setState({likesCount: newLikesCount});
+  }
 
-    render() {
-        return (
-            <div>
-                Likes : <span>{this.state.likesCount}</span>
-                <div><button onClick={this.onLike}>Like Me</button></div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        Likes : <span>{this.state.likesCount}</span>
+        <div>
+          <button onClick={this.onLike}>Like Me</button>
+        </div>
+      </div>
+    );
+  }
 
 }
 
